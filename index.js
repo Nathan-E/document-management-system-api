e4884a1b2b7b6c6a2eda3a873a75bf90eda49ad3
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 02:43:15 +0200

    create a port that the app runs on

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import express from 'express';
 
 const app = express();
 
+//middlewares
 app.use(express.json);
 
+//app PORT
+const port = process.env.PORT || 5000;
+
+//app listens on the specified PORT
+app.listen(port, () => {
+  console.log(`Listining on port ${port}`);
+})
