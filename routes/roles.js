f79b9bd27b7142ff65e9aaa26df409ea599e914c
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 01:23:14 +0200

    create swagger doc for get all roles route

--- routes/roles.js
+++ routes/roles.js
@@ -5,6 +5,23 @@ import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/roles:
+ *    get:
+ *      summary: gets all roles.
+ *      tags: [/api/v1/roles]
+ *      description: This should return all roles
+ *      responses:
+ *        200:
+ *          description: A list of roles
+ *          schema:
+ *            type: string
+ *        400:
+ *          description: Failed Request
+ *          schema:
+ *            type: string
+ */
 router.get('/', async (req, res) => {
   const roles = await Role.find().sort('title');
   
