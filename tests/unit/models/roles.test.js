a9883937d3816320b0871c51cb625830d9259f32
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 04:38:20 +0200

    create test case for role model

---
+++ tests/unit/models/roles.test.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill';
+import Role from '../../../models/roles';
+import mongoose from 'mongoose';
+import { exportAllDeclaration } from '@babel/types';
+
+describe('roles mongoose model', () => {
+  it('should return a valid role', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString();
+      title: 'Admin'
+    }
+
+    const newRole = new Role(payload);
+
+    expect(newRole).toBeInstanceOf(Role);
+    expect(newRole).toMatchObject(payload);
+  });
+});
