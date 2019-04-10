b820249684d3e8284dfb6cd09a3ccd353785f337
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 17:21:33 +0200

    create database connection logic in index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+const db = process.env.DATABASE;
 
 //middlewares
 app.use(express.json);
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log(`Connected to ${db}...`));
+
 
 //app PORT
 const port = process.env.PORT;
@@ -14,3 +23,5 @@ const port = process.env.PORT;
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port), () => console.log(`Listening on port ${port}...`);
 }
+
+export default app;
