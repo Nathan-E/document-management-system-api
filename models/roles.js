cadbe9e6bc7239bb135fe75ae99d66b49a1cb86f
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 05:01:22 +0200

    create role schema in models folder

---
+++ models/roles.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const roleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    min: 4,
+    max: 10,
+    unique: true,
+    required: true
+  }
+});
