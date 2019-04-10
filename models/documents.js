9dcbdb490bf1f022cf6a90d87494518d166cb40b
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 14:24:49 +0200

    create document mongoose schema

---
+++ models/documents.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const newDocument = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 10,
+    maxlength: 255
+  },
+  type: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId(),
+      ref: 'Type',
+      required: true
+    }
+  },
+  owner: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId(),
+      ref: 'User',
+      required: true
+    }
+  },
+  ownerRole: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    minlength: 10,
+    maxlength: 1024,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  accessRight: {
+    type: String,
+    default: 'public'
+  },
+  modifiedAt: {
+    type: Date,
+    default: null
+  },
+  userStatus: {
+    type: Boolean,
+    default: true
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  }
+});
