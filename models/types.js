e4f48cb84dd5736b3e20051d97b36d00f038e8eb
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 05:49:52 +0200

    create document types mongoose  schema

---
+++ models/types.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const typeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    min: 5,
+    max: 50,
+    unique: true,
+    required: true
+  }
+});
