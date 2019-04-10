5323824b790921da66602955eb12602d939e643f
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 05:29:06 +0200

    refactor roles.test.js file in tests/unit/models folder

--- tests/unit/models/roles.test.js
+++ tests/unit/models/roles.test.js
@@ -1,12 +1,11 @@
 import 'babel-polyfill';
-import Role from '../../../models/roles';
+import { Role } from '../../../models/roles';
 import mongoose from 'mongoose';
-import { exportAllDeclaration } from '@babel/types';
 
 describe('roles mongoose model', () => {
   it('should return a valid role', () => {
     const payload = {
-      _id: new mongoose.Types.ObjectId().toHexString(),
+      _id: new mongoose.Types.ObjectId(),
       title: 'Admin'
     }
 
