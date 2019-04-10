7192976f60ee8b919ced5ec7d09ff47c1b3d72e3
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 05:56:15 +0200

    create a validate function for type mongoose schema

--- models/types.js
+++ models/types.js
@@ -5,10 +5,18 @@ const typeSchema = new mongoose.Schema({
   title: {
     type: String,
     min: 5,
-    max: 50,
+    max: 25,
     unique: true,
     required: true
   }
 });
 
 const Type = mongoose.model('Type', typeSchema);
+
+const validate = role => {
+  const schema = {
+    title: Joi.string().min(5).max(25).required()
+  }
+
+  return Joi.validate(typeSchema, schema);
+}
