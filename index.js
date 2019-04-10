04cbc799e33293eb36cdfb937b6ce5d7b8a6cd4b
Nathan-E <eziokwubunduchibueze.com>
2019-04-10 17:48:09 +0200

    define environment variables

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const db = process.env.DATABASE;
+
+//environment variable
+let db = process.env.DATABASE;
+let jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
+
+if (process.env.NODE_ENV === test) db = process.env.TEST_DATABASE;
 
 //middlewares
 app.use(express.json);
 
+
+//database connection
 mongoose.connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
   })
   .then(() => console.log(`Connected to ${db}...`));
 
+if (!jwtPrivateKey) {
+  throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+}
 
 //app PORT
 const port = process.env.PORT;
