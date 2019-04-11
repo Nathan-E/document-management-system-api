5e6b66c7fecf3699c7655aa8f874bcb45ac74e79
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 17:26:01 +0200

    add type resources put request controller

--- controllers/types.js
+++ controllers/types.js
@@ -31,4 +31,30 @@ typeController.post = async (req, res) => {
   res.status(200).send('New type created!!!')
 };
 
+typeController.put = async (req, res) => {
+  const {
+    error
+  } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let type = await Type.findOne({
+    title: req.body.title,
+  });
+  if (type) return res.status(400).send(`${req.body.title} already exist`);
+
+
+  type = await Type.findOneAndUpdate({
+    _id: req.params.id
+  }, {
+    $set: {
+      title: req.body.title
+    }
+  }, {
+    new: true
+  });
+  if (!type) return res.status(404).send('The type with the given ID was not found.');
+
+  res.status(200).send(type);
+}
+
 export { typeController };
