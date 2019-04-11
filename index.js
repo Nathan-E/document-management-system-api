7d5e6d0bb09ce640511dd725b36aba197e4d5df8
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 02:04:05 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const port = process.env.PORT;
 
 //app listens on the specified PORT
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(port), () => console.log(`Listening on port ${port}...`);
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
 }
 
 export default app;
