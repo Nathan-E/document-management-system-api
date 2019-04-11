4e4d852ea17a037d4f264ad80451b16b31913283
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 19:31:41 +0200

    create role resource put request controller

--- controllers/roles.js
+++ controllers/roles.js
@@ -9,7 +9,7 @@ roleController.get = async (req, res) => {
   const role = await Role.find().sort('title');
 
   res.send(role);
-}
+};
 
 roleController.post = async (req, res) => {
   const {
@@ -29,6 +29,32 @@ roleController.post = async (req, res) => {
   await role.save();
 
   res.status(200).send('New role created!!!')
-}
+};
+
+roleController.put = async (req, res) => {
+  const {
+    error
+  } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let role = await Role.findOne({
+    title: req.body.title,
+  });
+  if (role) return res.status(400).send(`${req.body.title} already exist`);
+
+
+  role = await Role.findOneAndUpdate({
+    _id: req.params.id
+  }, {
+    $set: {
+      title: req.body.title
+    }
+  }, {
+    new: true
+  });
+  if (!role) return res.status(404).send('The role with the given ID was not found.');
+
+  res.status(200).send(role);
+};
 
 export { roleController };
