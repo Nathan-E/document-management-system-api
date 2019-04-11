a5f1b488e5b4633d3a519fbb82d82ed01f3c0c30
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 13:25:40 +0200

    add swagger doc for type resource get request endpoint

--- routes/types.js
+++ routes/types.js
@@ -4,6 +4,23 @@ import validateObjectId from '../middlewares/validateObjectId';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/type:
+ *    get:
+ *      summary: gets all role.
+ *      tags: [/api/v1/types]
+ *      description: This should return all types
+ *      responses:
+ *        200:
+ *          description: A list of types
+ *          schema:
+ *            type: string
+ *        400:
+ *          description: Failed Request
+ *          schema:
+ *            type: string
+ */
 router.get('/', async (req, res) => {
   const type = await Type.find().sort('title');
 
