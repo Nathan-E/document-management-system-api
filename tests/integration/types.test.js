ddafb3904e8a5eeeda6f13a0787eafbe1eda9caf
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 14:33:19 +0200

    add more integration test case for type resource

--- tests/integration/types.test.js
+++ tests/integration/types.test.js
@@ -107,6 +107,16 @@ describe('/api/v1/types', () => {
       expect(response.body).toHaveProperty('_id');
       expect(response.body).toHaveProperty('title', newTitle);
     });
+    it('should return 404 if an invalid id is passed', async () => {
+      const id = 1;
+      const newTitle = 'social';
+
+      const response = await request(server).put(`/api/v1/types/${id}`).send({
+        title: newTitle
+      });
+
+      expect(response.status).toBe(404);
+    });
   });
   describe('GET /:id', () => {
     it('should return an existing type', async () => {
