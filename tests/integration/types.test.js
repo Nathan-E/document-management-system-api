41a79233f714d6ddf43a7a6bd24c9b054e674912
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 13:16:42 +0200

    create integration test case for type resources get request

--- tests/integration/types.test.js
+++ tests/integration/types.test.js
@@ -1,7 +1,45 @@
 import 'babel-polyfill';
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { Type } from '../../models/types';
+import {
+  Type
+} from '../../models/types';
 import app from '../../index';
 
 let server
+
+describe('/api/v1/types', () => {
+  beforeAll(() => {
+    server = app;
+  });
+  beforeEach(() => {
+
+  });
+  afterEach(() => {
+
+  });
+  afterAll(async () => {
+    await Type.deleteMany();
+    server.close();
+  });
+  describe('GET /', () => {
+    it('should return all the roles', async () => {
+      await Type.collection.bulkWrite([{
+        insertOne: {
+          title: 'journal'
+        }
+      }, {
+        insertOne: {
+          title: 'fashion'
+        },
+      }]);
+
+      const response = await request(server).get('/api/v1/types');
+
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(2);
+      expect(response.body.some(g => g.title === 'journal')).toBeTruthy();
+      expect(response.body.some(g => g.title === 'fashion')).toBeTruthy();
+    });
+  });
+});
