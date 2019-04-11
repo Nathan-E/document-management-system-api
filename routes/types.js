e7fc3af374d75c2b109be84d60be5c3c3d865f2f
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 17:26:20 +0200

    refactor type put request router

--- routes/types.js
+++ routes/types.js
@@ -103,31 +103,7 @@ router.post('/', [auth], typeController.post);
  *          schema:
  *            type: string
  */
-router.put('/:id', [validateObjectId, auth, isAdmin], async (req, res) => {
-  const {
-    error
-  } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  let type = await Type.findOne({
-    title: req.body.title,
-  });
-  if (type) return res.status(400).send(`${req.body.title} already exist`);
-
-
-  type = await Type.findOneAndUpdate({
-    _id: req.params.id
-  }, {
-    $set: {
-      title: req.body.title
-    }
-  }, {
-    new: true
-  });
-  if (!type) return res.status(404).send('The type with the given ID was not found.');
-
-  res.status(200).send(type);
-});
+router.put('/:id', [validateObjectId, auth, isAdmin], typeController.put);
 
 /**
  * @swagger
