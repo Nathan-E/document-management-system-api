faa00ecec513b78b1c6a6babceb7c1fc16bc607c
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 13:35:17 +0200

    create post request endpoint for type resource

--- routes/types.js
+++ routes/types.js
@@ -27,4 +27,24 @@ router.get('/', async (req, res) => {
   res.send(type);
 });
 
+router.post('/', async (req, res) => {
+  const {
+    error
+  } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let type = await Type.findOne({
+    title: req.body.title
+  });
+  if (type) return res.status(400).send(`${req.body.title} already exist`);
+
+  type = new Type({
+    title: req.body.title
+  });
+
+  await type.save();
+
+  res.status(200).send('New type created!!!')
+});
+
 export { router as typesRouter }
