fde7e917c8965e06cf4f9d7e911857f4133a3471
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 19:29:54 +0200

    refactor role resource route

--- routes/roles.js
+++ routes/roles.js
@@ -60,25 +60,7 @@ router.get('/', [auth, isAdmin], roleController.get);
  *          schema:
  *            type: string
  */
-router.post('/', [auth, isAdmin],async (req, res) => {
-  const {
-    error
-  } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  let role = await Role.findOne({
-    title: req.body.title
-  });
-  if (role) return res.status(400).send(`${req.body.title} already exist`);
-
-  role = new Role({
-    title: req.body.title
-  });
-
-  await role.save();
-
-  res.status(200).send('New role created!!!')
-});
+router.post('/', [auth, isAdmin], roleController.post);
 
 /**
  * @swagger
