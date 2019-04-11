9a55088ba568073cdfc658e9d505e444178fae31
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 12:51:58 +0200

    refactor role resource intgeration test case

--- tests/integration/roles.test.js
+++ tests/integration/roles.test.js
@@ -32,7 +32,7 @@ describe('/api/v1/roles', () => {
     it('should return all the roles', async () => {
       await Role.collection.bulkWrite([{
         insertOne: {
-          title: 'admin'
+          title: 'Sadmin'
         }
       }, {
         insertOne: {
@@ -44,7 +44,7 @@ describe('/api/v1/roles', () => {
 
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(2);
-      expect(response.body.some(g => g.title === 'admin')).toBeTruthy();
+      expect(response.body.some(g => g.title === 'Sadmin')).toBeTruthy();
       expect(response.body.some(g => g.title === 'regular')).toBeTruthy();
     });
   });
@@ -64,11 +64,15 @@ describe('/api/v1/roles', () => {
       expect(response.status).toBe(200);
     });
     it('should return 400 if role already exist', async () => {
-      const roles = [{
+      const roles = {
         title: 'admin'
-      }, {
-        title: 'admin'
-      }];
+      }
+
+      await Role.collection.bulkWrite([{
+        insertOne: {
+          title: 'admin'
+        }
+      }]);
 
       const response = await request(server).post('/api/v1/roles').send(roles);
       expect(response.status).toBe(400);
