dcc9ab506097cc6daf2a01e66d77bab627176a85
Nathan-E <eziokwubunduchibueze.com>
2019-04-11 20:55:55 +0200

    setup user routes file

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+import { userController } from '../controllers/index';
+import express from 'express';
+import { validateObjectId, auth, isAdmin } from '../middlewares/index';
+
+const router = express.Router();
+
+
+
+export { router };
