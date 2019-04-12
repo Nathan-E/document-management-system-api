8ed22f3b2453f6387eaad08c35c51b220f0c7185
Nathan-E <eziokwubunduchibueze.com>
2019-04-12 16:54:34 +0200

    add swagger doc to user resource route

--- routes/users.js
+++ routes/users.js
@@ -7,24 +7,105 @@ import {
   auth,
   isAdmin
 } from '../middlewares/index';
-import {
-  User,
-  validate
-} from '../models/users';
-import {
-  Role
-} from '../models/roles';
-import _ from 'lodash';
-import bcrypt from 'bcrypt';
-import Joi from 'joi';
+
 
 const router = express.Router();
 
-
+/**
+ * @swagger
+ * /api/v1/users:
+ *    post:
+ *      summary: create a new user.
+ *      tags: [/api/v1/users]
+ *      consumes:
+ *        - application/json
+ *      description: This should create a new user
+ *      parameters:
+ *        - in: body
+ *          name: payload
+ *          description: should contain the user's name, email and password, role, username.
+ *      schema:
+ *        type: object
+ *        required:
+ *          - name
+ *        properties:
+ *          username:
+ *            type: string
+ *          email:
+ *            type: string
+ *          password:
+ *            type: string
+ *          firstname:
+ *            type: string
+ *          lastname:
+ *            type: string
+ *          role:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: User created successfully
+ *          schema:
+ *            type: string
+ *        400:
+ *          description: Could not create a user
+ *          schema:
+ *            type: string
+ */
 router.post('/signup', userController.signup);
 
+/**
+ * @swagger
+ * /api/v1/users:
+ *    post:
+ *      summary: login a user.
+ *      tags: [/api/v1/users]
+ *      consumes:
+ *        - application/json
+ *      description: This should login a user
+ *      parameters:
+ *        - in: body
+ *          name: payload
+ *          description: should contain the user's email and password.
+ *      schema:
+ *        type: object
+ *        required:
+ *          - name
+ *        properties:
+ *          email:
+ *            type: string
+ *          password:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: User logged in successfully
+ *          schema:
+ *            type: string
+ *        400:
+ *          description: Could not login user
+ *          schema:
+ *            type: string
+ */
 router.post('/login', userController.login);
 
+/**
+ * @swagger
+ * /api/v1/users:
+ *    post:
+ *      summary: logout a user.
+ *      tags: [/api/v1/users]
+ *      consumes:
+ *        - application/json
+ *      description: This should logout a user
+ *      parameters:
+ *        - in: body
+ *          name: payload
+ *          description: should contain the user's email and password.
+ *      responses:
+ *        200:
+ *          description: User logged out
+ *          schema:
+ *            type: string
+ */
 router.post('/logout', userController.logout);
 
 
