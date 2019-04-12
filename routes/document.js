75630f92f6481aa914bcf2d92f4247a8a27bd8d3
Nathan-E <eziokwubunduchibueze.com>
2019-04-12 22:37:04 +0200

    export document resource router

--- routes/document.js
+++ routes/document.js
@@ -3,3 +3,13 @@ import { validateDocument } from '../validations/index';
 import bcrypt from 'bcrypt';
 import express from 'express';
 import { validateObjectId, auth, isAdmin } from '../middlewares/index';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+
+});
+
+export {
+  router as documentRouter
+}
