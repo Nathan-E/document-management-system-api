fc6c7d5a28eb8a500ad829760bb4b37cbe74ba04
Nathan-E <eziokwubunduchibueze.com>
2019-04-12 13:35:46 +0200

    refactor user resource put request route

--- routes/users.js
+++ routes/users.js
@@ -85,9 +85,9 @@ router.get('/', [auth, isAdmin], async (req, res) => {
   res.send(user);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', [auth], async (req, res) => {
   const user = await User.findById(req.params.id);
-  
+
   if (!user) return res.status(404).send('The user with the given ID was not found.');
 
   res.send(user);
