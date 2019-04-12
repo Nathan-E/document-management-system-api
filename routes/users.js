de98c721b71732ce6c9dc909578966bd36bd6f43
Nathan-E <eziokwubunduchibueze.com>
2019-04-12 02:25:26 +0200

    refactor user resource post request endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,40 @@
-import { userController } from '../controllers/index';
+import {
+  userController
+} from '../controllers/index';
 import express from 'express';
-import { validateObjectId, auth, isAdmin } from '../middlewares/index';
-import { User, validate } from '../models/users';
-import { Role } from '../models/roles';
+import {
+  validateObjectId,
+  auth,
+  isAdmin
+} from '../middlewares/index';
+import {
+  User,
+  validate
+} from '../models/users';
+import {
+  Role
+} from '../models/roles';
+import _ from 'lodash';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
-router.post('/', async(req, res) => {
-  const { error } = validate(req.body);
-  if(error) return res.status(400).send(error.details[0].message);
+router.post('/signup', async (req, res) => {
+  const {
+    error
+  } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({email: req.body.email});
-  if(user) return res.status(400).send('User already exist');
+  let user = await User.findOne({
+    email: req.body.email
+  });
+  if (user) return res.status(400).send('User already exist');
 
   const role = await Role.findById(req.body.role_id);
   if (!role) return res.status(400).send('Invalid role.');
+
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
 
   user = new User({
     firstname: req.body.firstname,
@@ -25,12 +45,14 @@ router.post('/', async(req, res) => {
     },
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: password
   })
 
   await user.save();
 
-  res.send(user);
+  res.send('New user created!!!');
 });
 
-export { router as usersRouter };
+export {
+  router as usersRouter
+};
