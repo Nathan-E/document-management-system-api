f8d235ef9ff568e1362012ed848b51bb80cf4277
Nathan-E <eziokwubunduchibueze.com>
2019-04-13 20:22:21 +0200

    refactor document resource get by id request endpoint

--- routes/document.js
+++ routes/document.js
@@ -114,7 +114,130 @@ router.get("/", auth, async (req, res) => {
   res.send(docs);
 });
 
+router.get("/:id", [validateObjectId, auth], async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (!user) return res.status(400).send("Invalid request");
 
+  const role = await Role.findById(user.role);
+
+  const userRoleInfo = await Access.findOne({
+    name: role.title
+  });
+  userRoleInfo.level = Object.values(userRoleInfo)[3]["level"];
+
+  let doc = await Document.findById(req.params.id);
+  if (!doc) return res.status(400).send("Document does not exist");
+
+  const access1 = doc.accessRight == 4;
+
+  const access2 = doc.accessRight == 3 && doc.owner_id == user._id;
+
+  const access3 = doc.ownerRole == role.title && accessRight == userRoleInfo.level;
+
+  const access4 = userRoleInfo.level == 1;
+
+  if(access1 || access2 || access3 || access4) return res.status(200).send(doc);
+});
+
+// router.put("/:id", [validateObjectId, auth], async (req, res) => {
+//   const { error } = validateDocumentUpdate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   let doc = await Document.findById(req.params.id);
+//   if (!doc || doc.deleted)
+//     return res.status(400).send("Document does not exist");
+
+//   const type = req.body.type
+//     ? await Type.findOne({
+//         title: req.body.type
+//       })
+//     : doc;
+//   if (!type) return res.status(400).send("Invalid document type");
+
+//   const user = await User.findById(req.user._id);
+//   if (!user) return res.status(400).send("Invalid request");
+
+//   const role = await Role.findBsyId(user.role);
+//   if (!role) return res.status(400).send("Invalid request");
+
+//   const userRoleInfo = await Access.findOne({
+//     name: role.title
+//   });
+//   if (!userRoleInfo) return res.status(400).send("Invalid request");
+
+//   let access;
+
+//   if (req.body.accessRight) {
+//     access = await Access.findOne({
+//       name: req.body.accessRight
+//     });
+
+//     access.level = Object.values(access)[3]["level"];
+//     userRoleInfo.level = Object.values(userRoleInfo)[3]["level"];
+
+//     if (access.level < userRoleInfo.level)
+//       return res.status(400).send("Invalid request");
+//   }
+
+//   const accessRight = access ? access.name : doc.accessRight;
+
+//   doc = await Document.findOneAndUpdate(
+//     {
+//       _id: req.params.id
+//     },
+//     {
+//       $set: {
+//         title: req.body.title || doc.title,
+//         type_id: type._id,
+//         content: req.body.content || doc.content,
+//         accessRight: accessRight,
+//         modifiedAt: Date.now()
+//       }
+//     },
+//     {
+//       new: true
+//     }
+//   );
+
+//   res.send(doc);
+// });
+
+// router.delete("/:id", [validateObjectId, auth, isAdmin], async (req, res) => {
+//   let doc = await Document.findById(req.params.id);
+//   if (!doc || doc.deleted)
+//     return res.status(400).send("Document does not exist");
+
+//   doc = await Document.findOneAndUpdate(
+//     {
+//       _id: req.params.id
+//     },
+//     {
+//       $set: {
+//         deleted: true
+//       }
+//     },
+//     {
+//       new: true
+//     }
+//   );
+
+//   res.status(200).send(doc);
+// });
+
+//Validates the document fields
+function validateDocumentUpdate(document) {
+  const schema = {
+    title: Joi.string(),
+    type: Joi.string(),
+    content: Joi.string(),
+    accessRight: Joi.string(),
+    modifiedAt: Joi.date(),
+    userStatus: Joi.boolean(),
+    deleted: Joi.boolean()
+  };
+
+  return Joi.validate(document, schema);
+}
 
 export {
   router as documentRouter
