f9d153275a85109d9a043d3db45e93487b66248b
Nathan-E <eziokwubunduchibueze.com>
2019-04-13 20:54:18 +0200

    refactor document resource put request endpoint

--- routes/document.js
+++ routes/document.js
@@ -139,68 +139,70 @@ router.get("/:id", [validateObjectId, auth], async (req, res) => {
   if(access1 || access2 || access3 || access4) return res.status(200).send(doc);
 });
 
-// router.put("/:id", [validateObjectId, auth], async (req, res) => {
-//   const { error } = validateDocumentUpdate(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
+router.put("/:id", [validateObjectId, auth], async (req, res) => {
+  const { error } = validateDocumentUpdate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-//   let doc = await Document.findById(req.params.id);
-//   if (!doc || doc.deleted)
-//     return res.status(400).send("Document does not exist");
+  let doc = await Document.findById(req.params.id);
+  if (!doc || doc.deleted)
+    return res.status(401).send("Document does not exist");
 
-//   const type = req.body.type
-//     ? await Type.findOne({
-//         title: req.body.type
-//       })
-//     : doc;
-//   if (!type) return res.status(400).send("Invalid document type");
+  const user = await User.findById(req.user._id);
+  const compareObjectId = doc.owner_id.toString() === user._id.toString();
 
-//   const user = await User.findById(req.user._id);
-//   if (!user) return res.status(400).send("Invalid request");
+  if (!user || user.deleted || !compareObjectId) return res.status(402).send("Invalid request");
 
-//   const role = await Role.findBsyId(user.role);
-//   if (!role) return res.status(400).send("Invalid request");
+  const type = req.body.type
+    ? await Type.findOne({
+        title: req.body.type
+      })
+    : doc;
+  if (!type) return res.status(403).send("Invalid document type");
 
-//   const userRoleInfo = await Access.findOne({
-//     name: role.title
-//   });
-//   if (!userRoleInfo) return res.status(400).send("Invalid request");
+  const role = await Role.findById(user.role);
+  if (!role) return res.status(404).send("Invalid request");
 
-//   let access;
+  const userRoleInfo = await Access.findOne({
+    name: role.title
+  });
+  if (!userRoleInfo) return res.status(405).send("Invalid request");
 
-//   if (req.body.accessRight) {
-//     access = await Access.findOne({
-//       name: req.body.accessRight
-//     });
+  let access;
 
-//     access.level = Object.values(access)[3]["level"];
-//     userRoleInfo.level = Object.values(userRoleInfo)[3]["level"];
+  if (req.body.accessRight) {
+    access = await Access.findOne({
+      level: req.body.accessRight
+    });
 
-//     if (access.level < userRoleInfo.level)
-//       return res.status(400).send("Invalid request");
-//   }
+    access.level = Object.values(access)[3]["level"];
+    userRoleInfo.level = Object.values(userRoleInfo)[3]["level"];
 
-//   const accessRight = access ? access.name : doc.accessRight;
+    if (access.level < userRoleInfo.level)
+      return res.status(406).send("Invalid request");
+  }
 
-//   doc = await Document.findOneAndUpdate(
-//     {
-//       _id: req.params.id
-//     },
-//     {
-//       $set: {
-//         title: req.body.title || doc.title,
-//         type_id: type._id,
-//         content: req.body.content || doc.content,
-//         accessRight: accessRight,
-//         modifiedAt: Date.now()
-//       }
-//     },
-//     {
-//       new: true
-//     }
-//   );
+  const accessRight = access ? access.level : doc.accessRight;
 
-//   res.send(doc);
-// });
+  doc = await Document.findOneAndUpdate(
+    {
+      _id: req.params.id
+    },
+    {
+      $set: {
+        title: req.body.title || doc.title,
+        type_id: type._id,
+        content: req.body.content || doc.content,
+        accessRight: accessRight,
+        modifiedAt: Date.now()
+      }
+    },
+    {
+      new: true
+    }
+  );
+
+  res.send(doc);
+});
 
 // router.delete("/:id", [validateObjectId, auth, isAdmin], async (req, res) => {
 //   let doc = await Document.findById(req.params.id);
@@ -230,7 +232,7 @@ function validateDocumentUpdate(document) {
     title: Joi.string(),
     type: Joi.string(),
     content: Joi.string(),
-    accessRight: Joi.string(),
+    accessRight: Joi.number(),
     modifiedAt: Joi.date(),
     userStatus: Joi.boolean(),
     deleted: Joi.boolean()
