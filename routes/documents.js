ca65efd28ed497a6ec9b0cf648b9f74ba80bc237
Nathan-E <eziokwubunduchibueze.com>
2019-04-14 11:56:50 +0200

    add comment to codebase

--- routes/documents.js
+++ routes/documents.js
@@ -20,33 +20,35 @@ const router = express.Router();
 
 //creates a document
 router.post("/", auth, async (req, res) => {
+  //validates the document request body
   const {
     error
   } = validateDocument(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
+  //checks for the document type exist
   const type = await Type.findOne({
     title: req.body.type
   });
   if (!type) return res.status(404).send("Invalid document type1");
-
+  //chaecks if the user exist
   const user = await User.findById(req.user._id);
   if (!user) return res.status(404).send("Invalid request2");
-
+  //gets the user role
   const role = await Role.findById(user.role);
 
   const userRoleInfo = await Access.findOne({
     name: role.title
   });
-
+  //check for the access level passed in the request body
   const access = await Access.findOne({
     level: req.body.accessRight
   });
   if (!access) return res.status(404).send("Invalid request5");
 
+  //ensures the user does not assign to the document access levels above her
   if (access.level < userRoleInfo.level)
     return res.status(400).send("Invalid request");
-
+  //creates the document
   const document = new Document({
     title: req.body.title,
     type_id: type._id,
@@ -55,7 +57,7 @@ router.post("/", auth, async (req, res) => {
     content: req.body.content,
     accessRight: access.level
   });
-
+  //saves the document to the database
   await document.save();
 
   res.send("document created!!!");
@@ -63,23 +65,24 @@ router.post("/", auth, async (req, res) => {
 
 //Return document according to access level
 router.get("/", auth, async (req, res) => {
+  //get the request queries
   let page = req.query.page;
   let limit = req.query.limit;
+  //checks if the user exist
   const user = await User.findById(req.user._id);
   if (!user) return res.status(400).send("Invalid request");
-
+  //gets the role of the user
   const role = await Role.findById(user.role);
-
+  //checks the access level of the user
   const userRoleInfo = await Access.findOne({
     name: role.title
   });
-
+  //returns all documents if user is admin
   if (userRoleInfo.level === 1) {
     const doc = await Document.find();
-
     return res.status(200).send(doc);
   }
-
+  //returns the documents within the user access right
   const docs = await Document.find({
     $or: [{
         accessRight: 4
@@ -94,24 +97,25 @@ router.get("/", auth, async (req, res) => {
       }
     ]
   });
+  //checks if the query string is truthy
   if (!page) page = 0;
   page = Number(page);
   if (!limit) limit = 0;
   limit = Number(limit);
 
+  //set the pagination and limit
   let start = page * limit;
   let stop = start + limit;
 
   let chuncks;
-
+  //returns document in batches according to query string limit
   if (!start && limit) {
     chuncks = docs.slice(0, limit)
     return res.status(200).send(chuncks);
   }
-
+  //returns document in batches according to query string limit and page set
   if (start !=0 && limit) {
     chuncks = docs.slice(start, stop);
-    // console.log('hi');
     console.log(!limit, !page);
 
     return res.status(200).send(chuncks);
