6a6c37ec250f15174eb90cf1b72748ebc3dc50f1
Nathan-E <eziokwubunduchibueze.com>
2019-04-14 04:24:13 +0200

    add comments to code base WIP

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,17 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export default function (req, res, next) {
+//Authenication
+export default (req, res, next) => {
   const token = req.header('x-auth-token');
+  //return 401 if there is no token
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
+  //verifies the token send
   jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
     if (err) return res.status(400).send('Invalid token.');
 
+    //decodes the token
     req.user = decoded;
     next();
   });
