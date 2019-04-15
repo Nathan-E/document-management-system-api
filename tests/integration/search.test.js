32923501acc25fcc6780f69724ce9fc4671c5938
Nathan-E <eziokwubunduchibueze.com>
2019-04-15 16:05:01 +0200

    create more integration test caes for the search document resource  get request endpoint

--- tests/integration/search.test.js
+++ tests/integration/search.test.js
@@ -112,6 +112,22 @@ describe('/api/v1/search', () => {
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(1);
     });
+    it('should get all the document if the the admin requests for it and the accessRight and role query parameter are valid and invalid respectively', async () => {
+      const response = await request(server)
+        .get('/api/v1/search?accessRight=1&role=admin1')
+        .set('x-auth-token', adminToken)
+        .send();
+
+      expect(response.status).toBe(400);
+    });
+    it('should get all the document if the the admin requests for it and the role and accessRight query parameter are valid and invalid respectively', async () => {
+      const response = await request(server)
+        .get('/api/v1/search?accessRight=5&role=admin')
+        .set('x-auth-token', adminToken)
+        .send();
+
+      expect(response.status).toBe(400);
+    });
     it('should return 400 if the role query parameter does not exist', async () => {
       const response = await request(server)
         .get('/api/v1/search?role=regular1')
