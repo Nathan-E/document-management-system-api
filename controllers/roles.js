8cff35e2028abb4e02de78aa9a4d5a57f4b18596
Nathan-E <eziokwubunduchibueze.com>
2019-04-22 15:00:18 +0200

    refactor role resource controller to use ES6 classes WIP

--- controllers/roles.js
+++ controllers/roles.js
@@ -8,14 +8,6 @@ import {
 //role controller object
 const roleController = {};
 
-//GET / controller
-//gets all role document
-roleController.get = async (req, res) => {
-  //get all role document and sort bu title
-  const role = await Role.find().sort('title');
-  //response
-  res.send(role);
-};
 
 //POST / controller
 // Creates a unique role
@@ -42,6 +34,48 @@ roleController.post = async (req, res) => {
   res.status(200).send('New role created!!!')
 };
 
+
+class RoleController {
+  constructor() {};
+
+  //GET / controller
+  //gets all role document
+  async get(req, res) {
+    //get all role document and sort bu title
+    const role = await Role.find().sort('title');
+    //response
+    res.send(role);
+  };
+  //PUT /:id controller
+  //Updates existing role
+  async put(req, res) {
+    //validates the request body
+    const {
+      error
+    } = validateRole(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    //checks if any role exist with the same title in request body
+    let role = await Role.findOne({
+      title: req.body.title,
+    });
+    if (role) return res.status(400).send(`${req.body.title} already exist`);
+
+    //find the role and updates it
+    role = await Role.findOneAndUpdate({
+      _id: req.params.id
+    }, {
+      $set: {
+        title: req.body.title
+      }
+    }, {
+      new: true
+    });
+    if (!role) return res.status(404).send('The role with the given ID was not found.');
+
+    res.status(200).send(role);
+  };
+
+};
 //PUT /:id controller
 //Updates existing role
 roleController.put = async (req, res) => {
