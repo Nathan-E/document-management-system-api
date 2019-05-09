1c5235dd6554fc1474e312f54a8ee1f46fefbf5a
Nathan-E <eziokwubunduchibueze.com>
2019-05-09 02:35:16 +0200

    apply DRY principle to codebase

--- db_seeder/seeder.js
+++ db_seeder/seeder.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 import {
-  Role,
-  Access,
-  Type,
-} from "../models/index";
-import {seed, roles, access, types} from './seeder_class';
+  populateDatabase
+} from './seeder_class';
 
+//set database to remote
 let db = process.env.REMOTE_DATABASE;
 
+//connects to the database
 mongoose.connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
@@ -17,22 +16,5 @@ mongoose.connect(db, {
     console.log(e.message)
   });
 
-async function populateDatabase() {
-  const role = await Role.find();
-  const accesses = await Access.find();
-  const type = await Type.find();
-  if (role.length <= 0) await Role.collection.insertMany(roles);
-  if (accesses.length <= 0) await Access.collection.insertMany(access);
-  if (type.length <= 0) await Type.collection.insertMany(types);
-
-  const seeder = new seed();
-  await seeder.seedUsers(20);
-  await seeder.seedDocuments(100);
-};
-
+//populates the database
 populateDatabase();
-
-
-export {
-  populateDatabase
-};
